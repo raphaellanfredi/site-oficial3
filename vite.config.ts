b5b42b4bf26614006987680f37bcf6534a46d539
raphaellanfredi <raphael.lanfredi@gmail.com>
2025-12-04 14:46:46 -0300

    Configure base path for GitHub Pages
    Set the base path for GitHub Pages deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 import { metaImagesPlugin } from "./vite-plugin-meta-images";
 
 export default defineConfig({
+  // caminho do projeto no GitHub Pages
+  base: "/site-oficial3/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
